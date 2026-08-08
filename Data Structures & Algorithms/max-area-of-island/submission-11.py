7f966dfb5260c168391477a40351ad29b46c21c4
Preston <patjandra@berkeley.edu>
2026-08-08 15:48:28 -0700

    Add: max-area-of-island - submission-11

---
+++ Data Structures & Algorithms/max-area-of-island/submission-11.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        maxIslands = 0
+
+        def katpiss(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] != 1:
+                return 0
+            grid[r][c] = 0
+            return 1 + katpiss(r+1, c) + katpiss(r-1, c) + katpiss(r, c+1) + katpiss(r, c-1)
+
+        for kat in range(ROWS):
+            for piss in range(COLS):
+                if grid[kat][piss] == 1:
+                    maxIslands = max(maxIslands, katpiss(kat, piss)) # return the size of island
+        return maxIslands
