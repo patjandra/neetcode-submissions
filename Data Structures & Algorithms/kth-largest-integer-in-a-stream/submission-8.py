f870ebe98b2cb76df0766551448a6aaff2790924
Preston <patjandra@berkeley.edu>
2026-08-16 16:02:36 -0700

    Add: kth-largest-integer-in-a-stream - submission-8

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-8.py
@@ -0,0 +1,12 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        heapq.heapify(nums)
+        self.heap = nums
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
