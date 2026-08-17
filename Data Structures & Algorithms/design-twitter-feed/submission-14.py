9fe2606ce46457bc96d9038446fb615ae59efa87
Preston <patjandra@berkeley.edu>
2026-08-16 22:06:42 -0700

    Add: design-twitter-feed - submission-14

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-14.py
@@ -0,0 +1,29 @@
+class Twitter:
+
+    def __init__(self):
+        self.followings = defaultdict(set) # user: [following]
+        self.posts = defaultdict(list) # user: [posts]
+        self.time = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.posts[userId].append([self.time, tweetId])
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        feed = []
+        feed.extend(self.posts[userId])
+        for user in self.followings[userId]:
+            feed.extend(self.posts[user])
+        heapq.heapify(feed)
+        while len(feed) > 10:
+            heapq.heappop(feed)
+        feed.sort(reverse=True)
+        return [x[1] for x in feed]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.followings[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId and followeeId in self.followings[followerId]:
+            self.followings[followerId].remove(followeeId)
