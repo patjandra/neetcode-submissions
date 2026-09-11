de9001e1758368c21d684461cafa4b5d68dbc681
Preston <patjandra@berkeley.edu>
2026-09-11 16:27:05 -0700

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sMap = {}
+        tMap = {}
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            sMap[s[i]] = sMap.get(s[i], 0) + 1
+            tMap[t[i]] = tMap.get(t[i], 0) + 1
+        print(sMap)
+        print(tMap)
+        return sMap == tMap
