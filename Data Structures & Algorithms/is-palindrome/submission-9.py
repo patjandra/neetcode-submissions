519e8b8bfb533019cdfa404ce000dcba988e657f
Preston <patjandra@berkeley.edu>
2026-09-12 12:34:36 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clearStr = ""
+        spaceless = s.replace(" ", "")
+        for i in spaceless:
+            if i.isalnum():
+                clearStr += i.lower()
+        reverse = clearStr[::-1]
+        return clearStr == reverse
